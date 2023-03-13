4c3fb30de984f79b02d60218d8924f9513e7d5f0
Александр Крюков <sashakryukovoff@mail.ru>
2023-03-13 16:28:55 +0400

    1.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss';
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 
 const App = () => {
     return (
         <div className='app'>
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageLazy/>}></Route>
+                    <Route path={'/about'} element={<AboutPageLazy/>}></Route>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
