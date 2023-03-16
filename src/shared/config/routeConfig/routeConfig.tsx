1c7aa7d35f0fe40a8994614b88633f78213b43d6
Александр Крюков <sashakryukovoff@mail.ru>
2023-03-16 23:59:40 +0400

    2.11

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
+
+
+export enum AppRotes {
+    MAIN = 'main',
+    ABOUT = 'about',
+}
+
+export const RoutePath: Record<AppRotes, string> = {
+    [AppRotes.MAIN]: '/',
+    [AppRotes.ABOUT]: '/about',
+}
+
+export const routeConfig: Record<AppRotes, RouteProps> = {
+    [AppRotes.MAIN]: {
+        path: RoutePath.main,
+        element: <MainPage/>
+    },
+    [AppRotes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage/>
+    },
+}
