672148f3c150d927cfc159ed8692185805f3d6c4
Александр Крюков <sashakryukovoff@mail.ru>
2023-03-17 17:41:45 +0400

    2.12

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, string | boolean>;
 
-export function classNames(currentClassName: string, mods: Mods, additional: string[]): string {
+export function classNames(currentClassName: string, mods?: Mods, additional?: string[]): string {
     return [
         currentClassName,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className, value]) => className)
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,23 @@
+import styles from './Navbar.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(styles.Navbar, {}, [className])}>
+            <div className={classNames(styles.links, {}, [])}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={styles.mainLink}>
+                    Главная
+                </AppLink>
+
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};
