5a00063fd6619b5e390ea09fa5c72939dc14e6ab
Александр Крюков <sashakryukovoff@mail.ru>
2023-03-19 16:38:23 +0400

    2.20

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,52 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            [],
+        )).toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'someClass hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: false },
+            [],
+        )).toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'someClass hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: undefined },
+            [],
+        )).toBe(expected);
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames(
+            'someClass',
+            {},
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+
+    test('with additional class and mods', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+});
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,10 +1,10 @@
 type Mods = Record<string, string | boolean>;
 
-export function classNames(currentClassName: string, mods?: Mods, additional?: string[]): string {
+export function classNames(currentClassName: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         currentClassName,
         ...additional.filter(Boolean),
-        Object.entries(mods)
+        ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className, value]) => className),
     ].join(' ');
