e91548b2fd2a06bf6749b1d625d7b3a65fe42718
Александр Крюков <sashakryukovoff@mail.ru>
2023-03-19 17:39:29 +0400

    2.21

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,12 +2,13 @@ import React, { Suspense } from 'react';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { Route, Routes } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => {
     const { t } = useTranslation();
 
     return (
-        <Suspense fallback={<div>{t('Загрузка...')}</div>}>
+        <Suspense fallback={<PageLoader />}>
             <Routes>
                 {Object.values(routeConfig).map(({ element, path }) => (
                     <Route
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRotes {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRotes, string> = {
     [AppRotes.MAIN]: '/',
     [AppRotes.ABOUT]: '/about',
+    [AppRotes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRotes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRotes, RouteProps> = {
     [AppRotes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRotes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
