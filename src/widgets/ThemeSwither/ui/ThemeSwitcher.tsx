7914d83ce63079b055ddc00704ba2d70a32fd363
dondarubin <dondarubin@gmail.com>
2024-04-18 02:33:10 +0400

    6.44

--- src/widgets/ThemeSwither/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwither/ui/ThemeSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import ThemeDark from 'shared/assets/icons/ThemeIcons/theme-dark.svg';
 import ThemeLight from 'shared/assets/icons/ThemeIcons/theme-light.svg';
-// import ThemeCloud from 'shared/assets/icons/ThemeIcons/theme-rain.svg';
 import { memo } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button';
 import styles from './ThemeSwitcher.module.scss';
